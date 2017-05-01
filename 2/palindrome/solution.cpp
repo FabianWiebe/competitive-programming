5b12f1ae1c2532e88f9d8da2f91c2ceb07f791a8
Fabian Wiebe <fabian.wiebe@student.hpi.de>
2017-05-01 16:53:17 +0200

    finished homework 2

--- 2/palindrome/solution.cpp
+++ 2/palindrome/solution.cpp
@@ -2,83 +2,83 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <iterator>
 
 int main (void) {
 	std::ios::sync_with_stdio(false);
-	size_t total_number;
-	size_t iterations = 0;
+	int total_number;
+	int iterations = 0;
 	std::cin >> total_number;
 	std::string line;
 	while (++iterations <= total_number && std::cin >> line) {
 		const size_t length = line.length();
-		if (length == 1) {
+		if (length < 2) {
 			std::cout << -1 << std::endl;
 			continue;
-		} else if (length == 0) {
-			std::cout << 0 << std::endl;
-			continue;
 		}
+
 		const bool odd = length & 1;
-		// std::vector<char> word (line.begin(), line.end());
+
+		const auto line_begin = line.begin();
+		const auto line_end = line.end();
+		const auto before_end = line_end - 1;
+
 		size_t counter = 0;
-		auto down_itr = line.begin() + (length - 1) / 2;
-		auto up_itr = down_itr + 1;
+		bool current_single_middle = odd;
+		bool current_up = true;
+		bool current_single_middle_up = current_single_middle;
+		bool current_single_middle_down = !current_single_middle;
+		auto up_itr = line_begin + length / 2;
+		auto down_itr = up_itr - 1;
+
 		auto tmp_up_itr = up_itr;
 		auto tmp_down_itr = down_itr;
-		bool current_up = false;
+
+
 		size_t tmp_loop_counter = 0;
-		size_t tmp_length = odd ? length : length ^ 1;
-		while (down_itr != line.begin() && counter < tmp_length - 2 * tmp_loop_counter) {
+		while (up_itr != line_end) {
 			size_t tmp_counter = 1;
-			if (current_up) {
-				tmp_up_itr = up_itr;
-				tmp_down_itr = up_itr;
-				++up_itr;
-			} else {
-				tmp_up_itr = down_itr;
-				tmp_down_itr = down_itr;
-				--down_itr;
+			current_up = !current_up;
+			if (tmp_loop_counter + counter >= length) {
+				break;
 			}
-			if (current_up != odd) {
+			if (current_up == odd) { 
 				++tmp_loop_counter;
 			}
-			current_up = !current_up;
-			while (*--tmp_down_itr == *++tmp_up_itr) {
-				if (tmp_up_itr == line.end()) {
-					break;
+			if (!current_up) {
+				current_single_middle_up = !current_single_middle_up;
+				if (!current_single_middle_up) {
+					tmp_down_itr = up_itr;
+					tmp_up_itr = up_itr;
+					++up_itr;
+				} else {
+					tmp_down_itr = up_itr - 1;
+					tmp_up_itr = up_itr;
+					if (*tmp_down_itr == *tmp_up_itr) {
+						tmp_counter = 2;
+					} else {
+						continue;
+					}
 				}
-				tmp_counter += 2;
-			}
-			if (tmp_counter > counter) {
-				counter = tmp_counter;
-			}
-		}
-		down_itr = line.begin() + (length - 1) / 2;
-		up_itr = down_itr;
-		current_up = true;
-		// std::cout << line << std::endl;
-		auto line_last = --line.end();
-		while(down_itr != line.begin() || (!odd && (up_itr != line_last))) {
-			size_t tmp_counter = 0;
-			// std::cout << "up_itr: " << *up_itr << "  down_itr: " << *down_itr << "   current_up " << current_up << std::endl;
-			if (current_up) {
-				tmp_down_itr = up_itr;
-				++up_itr;
-				tmp_up_itr = up_itr;
 			} else {
-				tmp_up_itr = down_itr;
-				--down_itr;
-				tmp_down_itr = down_itr;
-			}
-			// std::cout << "up_itr: " << *up_itr << "  down_itr: " << *down_itr  << std::endl;
-			current_up = !current_up;
-			while (*tmp_down_itr == *tmp_up_itr) {
-				--tmp_down_itr;
-				++tmp_up_itr;
-				tmp_counter += 2;
-				if (tmp_up_itr == line.end()) {
-					break;
+				current_single_middle_down = !current_single_middle_down;
+				if (!current_single_middle_down) {
+					tmp_down_itr = down_itr;
+					tmp_up_itr = down_itr;
+					--down_itr;
+				} else {
+					tmp_down_itr = down_itr;
+					tmp_up_itr = down_itr + 1;
+					if (*tmp_down_itr == *tmp_up_itr) {
+						tmp_counter = 2;
+					} else {
+						continue;
+					}
 				}
+			}
+
+			while (tmp_down_itr != line_begin && tmp_up_itr != before_end && *--tmp_down_itr == *++tmp_up_itr) {
+				tmp_counter += 2;
 			}
 			if (tmp_counter > counter) {
 				counter = tmp_counter;
